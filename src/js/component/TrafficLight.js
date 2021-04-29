e02a35d37af3d86968397129ae1aa3973dcb41f5
Valentina Cuello <delmar_vale08@hotmail.com>
2021-04-29 12:11:00 +0000

    Ejercicio pronto

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -2,17 +2,9 @@ import React, { useState } from "react";
 import "../../styles/index.scss";
 
 const TrafficLight = () => {
-	const [luz, setLuz] = useState("off"); //es el estado inicial de luz
-	//luz es el estado (es una variable que guarda algo que se actualiza)
-	//setLuz es una función que actualiza el estado (o sea, luz)
+	const [luz, setLuz] = useState("off");
 
 	const prenderLuz = color => setLuz(color);
-
-	//La función de arriba sería lo mismo que escribir lo de acá debajo
-	// function prenderLuz(color){
-	//     setLuz(color)
-	// };
-	//Declaro una función "prenderLuz()" a quién le paso el parámetro color, dentro llamo a la función setLuz pasándole ese parámetro
 
 	return (
 		<div className="traffic-light-container">
@@ -28,10 +20,7 @@ const TrafficLight = () => {
 					<div className="yellow-glow"></div>
 				</div>
 				<div
-                    className={luz == "g" ? "green encendida" : "green"}//acá planteo un ternario donde pregunto si el estado luz
-                    //es igual a "g" (parametro que le paso a la función), si ese parámetro es true, entonces mediante la clase
-                    //encendida en css le agrego un display block a las clases que tienen el "glow", sino, esa clase no se le agrega
-                    //queda solo la clase green (o yellow o red)
+					className={luz == "g" ? "green encendida" : "green"}
 					onClick={() => prenderLuz("g")}>
 					<div className="green-glow"></div>
 				</div>
